1cc52c314a6b9517fb2dbe278a651a330fd2d6e9
Aleksandra Simic <aleksandraduo@gmail.com>
2021-10-31 18:53:25 +0100

    Yomeals Project

---
+++ src/pages/Basic_Page.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class Basic_Page {
+
+	private WebDriver driver;
+
+	public Basic_Page(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+}
---
+++ src/pages/Location_Popup_Page.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Location_Popup_Page extends Basic_Page {
+
+	public Location_Popup_Page(WebDriver driver) {
+		super(driver);
+	}
+
+}
